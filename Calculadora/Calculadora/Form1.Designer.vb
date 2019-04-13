73e3998e6b04de12fb885ce64e29a5733510f7a4
Jeanmillette <47704780+glendagarcia@users.noreply.github.com>
2019-04-13 03:21:15 -0600

    Primer Commit de Calculadora en Windows Form

---
+++ Calculadora/Calculadora/Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnSuma = New System.Windows.Forms.Button()
+        Me.btnResta = New System.Windows.Forms.Button()
+        Me.btnMultiplicacion = New System.Windows.Forms.Button()
+        Me.btnDivision = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.limpiar = New System.Windows.Forms.Button()
+        Me.txtnum1 = New System.Windows.Forms.TextBox()
+        Me.txtnum2 = New System.Windows.Forms.TextBox()
+        Me.txtresultado = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ingresar numeros:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Número 1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 90)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Número 2"
+        '
+        'btnSuma
+        '
+        Me.btnSuma.Location = New System.Drawing.Point(336, 28)
+        Me.btnSuma.Name = "btnSuma"
+        Me.btnSuma.Size = New System.Drawing.Size(123, 35)
+        Me.btnSuma.TabIndex = 5
+        Me.btnSuma.Text = "Suma"
+        Me.btnSuma.UseVisualStyleBackColor = True
+        '
+        'btnResta
+        '
+        Me.btnResta.Location = New System.Drawing.Point(336, 69)
+        Me.btnResta.Name = "btnResta"
+        Me.btnResta.Size = New System.Drawing.Size(123, 34)
+        Me.btnResta.TabIndex = 6
+        Me.btnResta.Text = "Resta"
+        Me.btnResta.UseVisualStyleBackColor = True
+        '
+        'btnMultiplicacion
+        '
+        Me.btnMultiplicacion.Location = New System.Drawing.Point(336, 109)
+        Me.btnMultiplicacion.Name = "btnMultiplicacion"
+        Me.btnMultiplicacion.Size = New System.Drawing.Size(123, 38)
+        Me.btnMultiplicacion.TabIndex = 7
+        Me.btnMultiplicacion.Text = "Multiplicación"
+        Me.btnMultiplicacion.UseVisualStyleBackColor = True
+        '
+        'btnDivision
+        '
+        Me.btnDivision.Location = New System.Drawing.Point(336, 154)
+        Me.btnDivision.Name = "btnDivision"
+        Me.btnDivision.Size = New System.Drawing.Size(123, 35)
+        Me.btnDivision.TabIndex = 8
+        Me.btnDivision.Text = "División"
+        Me.btnDivision.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 134)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Resultado"
+        '
+        'limpiar
+        '
+        Me.limpiar.Location = New System.Drawing.Point(9, 163)
+        Me.limpiar.Name = "limpiar"
+        Me.limpiar.Size = New System.Drawing.Size(123, 26)
+        Me.limpiar.TabIndex = 12
+        Me.limpiar.Text = "Limpiar"
+        Me.limpiar.UseVisualStyleBackColor = True
+        '
+        'txtnum1
+        '
+        Me.txtnum1.Location = New System.Drawing.Point(82, 55)
+        Me.txtnum1.Name = "txtnum1"
+        Me.txtnum1.Size = New System.Drawing.Size(217, 20)
+        Me.txtnum1.TabIndex = 13
+        '
+        'txtnum2
+        '
+        Me.txtnum2.Location = New System.Drawing.Point(82, 90)
+        Me.txtnum2.Name = "txtnum2"
+        Me.txtnum2.Size = New System.Drawing.Size(217, 20)
+        Me.txtnum2.TabIndex = 14
+        '
+        'txtresultado
+        '
+        Me.txtresultado.Location = New System.Drawing.Point(82, 131)
+        Me.txtresultado.Name = "txtresultado"
+        Me.txtresultado.Size = New System.Drawing.Size(217, 20)
+        Me.txtresultado.TabIndex = 15
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(471, 204)
+        Me.Controls.Add(Me.txtresultado)
+        Me.Controls.Add(Me.txtnum2)
+        Me.Controls.Add(Me.txtnum1)
+        Me.Controls.Add(Me.limpiar)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnDivision)
+        Me.Controls.Add(Me.btnMultiplicacion)
+        Me.Controls.Add(Me.btnResta)
+        Me.Controls.Add(Me.btnSuma)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnSuma As Button
+    Friend WithEvents btnResta As Button
+    Friend WithEvents btnMultiplicacion As Button
+    Friend WithEvents btnDivision As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents limpiar As Button
+    Friend WithEvents txtnum1 As TextBox
+    Friend WithEvents txtnum2 As TextBox
+    Friend WithEvents txtresultado As TextBox
+End Class
---
+++ Calculadora/Calculadora/Form1.vb
@@ -0,0 +1,65 @@
+﻿Public Class Form1
+    Private Sub limpriar()
+        txtnum1.Clear()
+        txtnum2.Clear()
+        txtresultado.Clear()
+    End Sub
+    Function operaciones(num1 As Integer, num2 As Integer, opc As String)
+        Dim resultado As Double
+        Select Case opc
+            Case "S"
+                resultado = num1 + num2
+            Case "R"
+                resultado = num1 - num2
+            Case "M"
+                resultado = num1 * num2
+            Case "D"
+                resultado = num1 / num2
+        End Select
+        Return resultado
+    End Function
+
+    Private Sub limpiar_Click(sender As Object, e As EventArgs) Handles limpiar.Click
+        limpriar()
+    End Sub
+
+    Private Sub btnSuma_Click(sender As Object, e As EventArgs) Handles btnSuma.Click
+        Dim num1 As Integer
+        Dim num2 As Integer
+        Dim resultado As Double
+        num1 = Val(txtnum1.Text)
+        num2 = Val(txtnum2.Text)
+        resultado = operaciones(num1, num2, "S")
+        txtresultado.Text = resultado
+    End Sub
+
+    Private Sub btnResta_Click(sender As Object, e As EventArgs) Handles btnResta.Click
+        Dim num1 As Integer
+        Dim num2 As Integer
+        Dim resultado As Double
+        num1 = Val(txtnum1.Text)
+        num2 = Val(txtnum2.Text)
+        resultado = operaciones(num1, num2, "R")
+        txtresultado.Text = resultado
+    End Sub
+
+    Private Sub btnMultiplicacion_Click(sender As Object, e As EventArgs) Handles btnMultiplicacion.Click
+        Dim num1 As Integer
+        Dim num2 As Integer
+        Dim resultado As Double
+        num1 = Val(txtnum1.Text)
+        num2 = Val(txtnum2.Text)
+        resultado = operaciones(num1, num2, "M")
+        txtresultado.Text = resultado
+    End Sub
+
+    Private Sub btnDivision_Click(sender As Object, e As EventArgs) Handles btnDivision.Click
+        Dim num1 As Integer
+        Dim num2 As Integer
+        Dim resultado As Double
+        num1 = Val(txtnum1.Text)
+        num2 = Val(txtnum2.Text)
+        resultado = operaciones(num1, num2, "D")
+        txtresultado.Text = resultado
+    End Sub
+End Class
